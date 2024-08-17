14d961342e56a191f250a71f3d7f1c2675efedf1
Chelsea91117 <aidana.latifova@yandex.kz>
2024-08-17 17:59:04 +0200

    Home work 11

--- first_app/urls.py
+++ first_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('greet/<str:name>/', views.greet, name='greet'),
+    path('greet/<str:name>/', greet, name='greet'),
+    path('tasks/', tasks_create_list, name='tasks-create'),
+    path('tasks/filter/', tasks_filter_by_status_and_deadline, name='tasks-filter-by-status-and-deadline'),
+    path('tasks/statistics/', tasks_statistics, name='tasks-statistics')
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,5 +1,64 @@
+from django.db.models import Count
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import *
+from django.utils import timezone
 
 
 def greet(request, name):
     return HttpResponse(f"<h1>Hello, {name}!</h1>")
+
+
+@api_view(['POST', 'GET'])
+def tasks_create_list(request):
+    if request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'GET':
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def tasks_filter_by_status_and_deadline(request):
+    status_filter = request.query_params.get('status')
+    deadline_filter = request.query_params.get('deadline')
+
+    tasks = Task.objects.all()
+
+    if status_filter:
+        tasks = tasks.filter(status=status_filter)
+
+    if deadline_filter:
+        tasks = tasks.filter(deadline__lte=deadline_filter)
+
+    paginator = PageNumberPagination()
+    paginator.page_size = 3
+    paginated_tasks = paginator.paginate_queryset(tasks, request)
+
+    serializer = TaskSerializer(paginated_tasks, many=True)
+    return paginator.get_paginated_response(serializer.data)
+
+
+@api_view(['GET'])
+def tasks_statistics(request):
+    total_tasks = Task.objects.count()
+    count_by_status = Task.objects.values('status').annotate(count=Count('id'))
+    overdue_tasks = Task.objects.filter(deadline__lt=timezone.now()).count()
+
+    response_data = {
+        'total_tasks': total_tasks,
+        'status_count': count_by_status,
+        'overdue_tasks': overdue_tasks
+    }
+
+    return Response(response_data)
