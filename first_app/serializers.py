72c986f4d6e2704ddcb7d63521f26e874edfa54e
Chelsea91117 <aidana.latifova@yandex.kz>
2024-08-19 20:22:50 +0200

    Home work 12

--- first_app/serializers.py
+++ first_app/serializers.py
@@ -1,8 +1,56 @@
 from rest_framework import serializers
+from django.utils import timezone
 from .models import *
+
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ['title', 'description', 'status', 'deadline']
+
+
+class SubTaskCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubTask
+        fields = '__all__'
+        read_only_fields = ['created_at']
+
+
+class CategoryCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+    def create(self, validated_data):
+        name = validated_data.get('name')
+        if Category.objects.filter(name=name).exists():
+            raise serializers.ValidationError('Категория с таким названием уже существует.')
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        name = validated_data.get('name')
+        if name != instance.name and Category.objects.filter(name=name).exists():
+            raise serializers.ValidationError('Категория с таким названием уже существует.')
+        return super().update(instance, validated_data)
+
+
+class TaskDetailSerializer(serializers.ModelSerializer):
+    subtask = SubTaskCreateSerializer()
+    class Meta:
+        model = Task
+        fields = '__all__'
+        read_only_fields = ['created_at']
+
+
+class TaskCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        read_only_fields = ['created_at']
+
+    def validate_deadline(self, value: str) -> int:
+        value = timezone.make_aware(value.replace(tzinfo=None), timezone.get_current_timezone())
+        if value < timezone.now():
+            raise serializers.ValidationError("Дедлайн не может быть в прошлом")
+        return value
+
 
--- first_app/urls.py
+++ first_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('greet/<str:name>/', greet, name='greet'),
     path('tasks/', tasks_create_list, name='tasks-create'),
     path('tasks/filter/', tasks_filter_by_status_and_deadline, name='tasks-filter-by-status-and-deadline'),
-    path('tasks/statistics/', tasks_statistics, name='tasks-statistics')
+    path('tasks/statistics/', tasks_statistics, name='tasks-statistics'),
+    path('subtasks/', SubTaskListCreateView.as_view(), name='subtask-list-or-create'),
+    path('subtasks/<int:pk>/', SubTaskDetailUpdateDeleteView.as_view(), name='subtask-update-or-delete'),
 ]
